08a083fa29d26e93256fab889d4d8bf38f73c21c
Dan Kelleher <dkelleher@g.harvard.edu>
2015-10-29 21:50:48 -0400

    Update permisions on one_auth

--- docker/one-controller/one-auth.sh
+++ docker/one-controller/one-auth.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-if [ -f /var/lib/one/.one/one_auth ]; then
+if [ -f ~/.one/one_auth ]; then
         echo "Password for one admin already exists"
     else
         ONE_PASSWORD=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
         
-        cat <<-EOT > /var/lib/one/.one/one_auth
+        cat <<-EOT > ~/.one/one_auth
 oneadmin:$ONE_PASSWORD
 EOT
         echo "The oneadmin password is $ONE_PASSWORD"
+        
+        chmod 600 ~/.one/one_auth
 fi
 
-if [ -f /var/lib/one/.ssh/id_dsa ]; then
+if [ -f ~/.ssh/id_dsa ]; then
         echo "DSA key already exists"
     else
         ssh-keygen -t dsa -f ~/.ssh/id_dsa -N ''
