3e0ccb72ed31eaec385c2fdaf89b035acb77fd23
Justin Riley <justin.t.riley@gmail.com>
2018-02-14 00:32:00 -0500

    novnc: add support for secured web socket (wss://)

--- docker/5.4/one-novnc/start.sh
+++ docker/5.4/one-novnc/start.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -o errexit
 
+# Configure SSL
+VNC_PROXY_SUPPORT_WSS=${VNC_PROXY_SUPPORT_WSS:-"no"}
+VNC_PROXY_CERT=${VNC_PROXY_CERT:-"/etc/pki/tls/certs/novnc.cer"}
+VNC_PROXY_KEY=${VNC_PROXY_KEY:-"/etc/pki/tls/private/novnc.key"}
+
+sed -i "s%^:vnc_proxy_support_wss:.*$%:vnc_proxy_support_wss: ${VNC_PROXY_SUPPORT_WSS}%g" /etc/one/sunstone-server.conf
+sed -i "s%^:vnc_proxy_cert:.*$%:vnc_proxy_cert: ${VNC_PROXY_CERT}%g" /etc/one/sunstone-server.conf
+sed -i "s%^:vnc_proxy_key:.*$%:vnc_proxy_key: ${VNC_PROXY_KEY}%g" /etc/one/sunstone-server.conf
+
 # Start supervisord
 /usr/bin/supervisord
 
