27e6490d925a5b13fa33b8665a3a5dfc423e1ac5
Justin Riley <justin.t.riley@gmail.com>
2018-02-25 09:15:02 -0500

    controller: use RSA for ssh key type

--- docker/5.4/one-controller/one-auth.sh
+++ docker/5.4/one-controller/one-auth.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
 if [ -f ~/.one/one_auth ]; then
-        echo "Password for one admin already exists"
-    else
-        ONE_PASSWORD=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
-        
-        cat <<-EOT > ~/.one/one_auth
-oneadmin:$ONE_PASSWORD
-EOT
-        echo "The oneadmin password is $ONE_PASSWORD"
-        
-        chmod 600 ~/.one/one_auth
+  echo "Password for one admin already exists"
+else
+  ONE_PASSWORD=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
+  echo "oneadmin:$ONE_PASSWORD" > ~/.one/one_auth
+  echo "The oneadmin password is $ONE_PASSWORD"
+  chmod 600 ~/.one/one_auth
 fi
 
-if [ -f ~/.ssh/id_dsa ]; then
-        echo "DSA key already exists"
-    else
-        ssh-keygen -t dsa -f ~/.ssh/id_dsa -N ''
-        
-        cat ~/.ssh/id_dsa.pub > ~/.ssh/authorized_keys
-        chmod 700 ~/.ssh
-        chmod 600 ~/.ssh/authorized_keys
+if [ -f ~/.ssh/id_rsa ]; then
+  echo "SSH RSA key already exists"
+else
+  ssh-keygen -t rsa -f ~/.ssh/id_rsa -N ''
+  cat ~/.ssh/id_rsa.pub > ~/.ssh/authorized_keys
+  chmod 700 ~/.ssh
+  chmod 600 ~/.ssh/authorized_keys
 fi
